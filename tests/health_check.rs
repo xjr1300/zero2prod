58112caed2279434eb8854be0138eaf9d4ccd2ba
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-08 16:14:54 +0900

    テストにおいて登録した購読者をデータベースから取得する処理を実装

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -29,7 +29,7 @@ async fn subscriber_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_string = configuration.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to postgres.");
     let client = reqwest::Client::new();
@@ -42,6 +42,13 @@ async fn subscriber_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request.");
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscriptions.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
