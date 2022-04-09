e19e03c440e2e3289db44dae58c41152753c474e
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-09 16:43:58 +0900

    ログを出力する機能を実装

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 
 use zero2prod::configuration::get_configuration;
@@ -7,6 +8,7 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
@@ -10,6 +9,7 @@ pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error
     let pool = web::Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(pool.clone())
