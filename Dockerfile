2e1f05a9c3172ebb5294af8fc693c681035dab16
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-11 16:12:26 +0900

    アプリをDockerで起動する機能を実装

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# ベースイメージとしてRustの安定版リリースを使用
+FROM rust:1.60.0
+
+# 存在しなければappディレクトリを作成して、appディレクトリに移動
+WORKDIR /app
+
+# リンクを構成するために必要とされているシステム依存をインストール
+RUN apt update && apt install lld clang -y
+
+# 作業環境からすべてのファイルをイメージ(/app)にコピー
+COPY . .
+
+# sqlxがクエリのメタデータをファイルから取得するようにフラグを設定
+ENV SQLX_OFFLINE true
+
+# バイナリを構築
+RUN cargo build --release
+
+# `docker run`が実行されたとき、バイナリを実行
+ENTRYPOINT ["./target/release/zero2prod"]
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,7 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let pool = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
+    let pool = PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
