1a851836e559ab2c5bf9e97259ced4f42171d55d
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-12 17:03:39 +0900

    is_valid_name関数を実装

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -18,6 +19,9 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(pool: web::Data<PgPool>, form: web::Form<FormData>) -> HttpResponse {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
     match insert_subscriber(&pool, &form.email, &form.name).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
@@ -44,4 +48,16 @@ pub async fn insert_subscriber(pool: &PgPool, email: &str, name: &str) -> Result
     })?;
 
     Ok(())
+}
+
+fn is_valid_name(s: &str) -> bool {
+    // 前後の空白文字をトリムした後に、文字列が空であるか確認
+    let is_empty_or_whitespace = s.trim().is_empty();
+    // 256文字以上か確認
+    let is_too_long = s.graphemes(true).count() > 256;
+    // 不適切な文字が含まれているか確認
+    let forbidden_chars = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_chars = s.chars().any(|g| forbidden_chars.contains(&g));
+
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_chars)
 }
