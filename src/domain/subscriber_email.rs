5886de9d620dcb8c9a054a88a1564721980dad14
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-13 08:51:38 +0900

    簡易にSubscriberEmail構造体を実装

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,1 +1,15 @@
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
 
+impl SubscriberEmail {
+    pub fn parse(s: &str) -> Result<Self, String> {
+        // TODO: add validation.
+        Ok(Self(s.to_owned()))
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
