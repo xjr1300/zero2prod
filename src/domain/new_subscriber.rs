8bd9e445898fdbf2cdb94b114265d852c8e6ecd2
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-13 11:14:55 +0900

    購読APIのテストを実装

--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -1,7 +1,8 @@
+use super::subscriber_email::SubscriberEmail;
 use super::subscriber_name::SubscriberName;
 
 #[derive(Debug)]
 pub struct NewSubscriber {
-    pub email: String,
+    pub email: SubscriberEmail,
     pub name: SubscriberName,
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,7 +3,7 @@ use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -20,14 +20,15 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(pool: web::Data<PgPool>, form: web::Form<FormData>) -> HttpResponse {
+    let email = match SubscriberEmail::parse(&form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     let name = match SubscriberName::parse(&form.0.name) {
         Ok(name) => name,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
-    };
+    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
@@ -48,7 +49,7 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
