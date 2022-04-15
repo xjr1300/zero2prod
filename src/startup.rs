f1f2a15bb2a2988f464d15d39fbe73b351ba8429
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-15 17:56:04 +0900

    簡単に購読確認APIを実装

--- src/startup.rs
+++ src/startup.rs
@@ -6,7 +6,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check, subscribe};
 
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
     PgPoolOptions::new()
@@ -26,6 +26,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("subscriptions/confirm", web::get().to(confirm))
             .app_data(pool.clone())
             .app_data(email_client.clone())
     })
