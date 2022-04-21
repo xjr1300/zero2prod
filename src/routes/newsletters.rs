200147eded7f65e3304aacda5a09c6a78965b9d9
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-21 11:40:18 +0900

    Basic認証の認証情報を取得する機能を実装

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,10 @@
-use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
+use actix_web::http::{
+    header::{HeaderMap, HeaderValue, WWW_AUTHENTICATE},
+    StatusCode,
+};
+use actix_web::{web, HttpRequest, HttpResponse, ResponseError};
 use anyhow::Context;
+use secrecy::Secret;
 use sqlx::PgPool;
 
 use crate::domain::SubscriberEmail;
@@ -8,8 +13,38 @@ use crate::routes::error_chain_fmt;
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication failed.")]
+    AuthError(#[source] anyhow::Error),
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
+}
+
+impl ResponseError for PublishError {
+    // `status_code`はデフォルト実装の`error_response`によって呼び出される。
+    // `error_response`の実装を提供することで、`status_code`の実装を維持する必要がなくなる。
+    // fn status_code(&self) -> StatusCode {
+    //     match self {
+    //         PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    //         PublishError::AuthError(_) => StatusCode::UNAUTHORIZED,
+    //     }
+    // }
+
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            PublishError::UnexpectedError(_) => {
+                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+            PublishError::AuthError(_) => {
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
+                response
+                    .headers_mut()
+                    .insert(WWW_AUTHENTICATE, header_value);
+
+                response
+            }
+        }
+    }
 }
 
 impl std::fmt::Debug for PublishError {
@@ -18,20 +53,21 @@ impl std::fmt::Debug for PublishError {
     }
 }
 
-impl ResponseError for PublishError {
-    fn status_code(&self) -> StatusCode {
-        match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-        }
-    }
-}
-
-/// ダミー実装
+#[tracing::instrument(
+    name = "Publish a newsletter issue", 
+    skip(body, pool, email_client, request),
+    fields(
+        username=tracing::field::Empty,
+        user_id=tracing::field::Empty,
+    )
+)]
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
+    request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
+    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
@@ -59,6 +95,43 @@ pub async fn publish_newsletter(
     }
 
     Ok(HttpResponse::Ok().finish())
+}
+
+struct Credentials {
+    username: String,
+    password: Secret<String>,
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    // ヘッダーの値があれば、それはUTF8文字列
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing.")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF8 string.")?;
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic: ")
+        .context("The authorization scheme wa not 'Basic'.")?;
+    let decoded_bytes = base64::decode_config(base64encoded_segment, base64::STANDARD)
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credential string is not valid UTF8.")?;
+
+    // ':'区切り文字を使用して、文字列を2つの部分に分割
+    let mut credentials = decoded_credentials.splitn(2, ':');
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
 }
 
 #[derive(serde::Deserialize)]
