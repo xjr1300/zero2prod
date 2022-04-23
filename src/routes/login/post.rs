c06adaba9fb88fc3357f8f71242240dddd93ce22
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-23 16:40:49 +0900

    型付けされたセッションに変更

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,4 +1,3 @@
-use actix_session::Session;
 use actix_web::{error::InternalError, http::header::LOCATION, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::Secret;
@@ -7,6 +6,7 @@ use sqlx::PgPool;
 use crate::{
     authentication::{validate_credentials, AuthError, Credentials},
     routes::error_chain_fmt,
+    session_state::TypedSession,
 };
 
 #[derive(serde::Deserialize)]
@@ -36,7 +36,7 @@ impl std::fmt::Debug for LoginError {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -48,7 +48,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
