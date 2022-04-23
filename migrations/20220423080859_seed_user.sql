797e471cb8b126f4da99a2009d318adbf438169c
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-23 17:19:48 +0900

    adminユーザーをデータベースに追加

---
+++ migrations/20220423080859_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+        'ba7c136e-5c72-460d-9ca8-81a935f58218',
+        'admin',
+        '$argon2id$v=19$m=15000,t=2,p=1$GZs6YXQ2BDClO8fEACSPFw$PYst3kxFFSIitTm0FMk4gCoigOGK0tzP4qaIcA9hUxI'
+    );
