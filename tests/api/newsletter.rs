e38cba954c0812c8a535e5bd00812b2219489acc
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-04-26 10:54:33 +0900

    ニュースレターEメールを重複して送信しないことを確認するテストを実装

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -120,3 +120,42 @@ async fn you_must_be_logged_in_to_publish_a_newsletter() {
 
     assert_is_redirect_to(&response, "/login");
 }
+
+#[tokio::test]
+async fn newsletter_creation_is_idempotent() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // ニュースレターフォームを提出
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text.",
+        "html_content": "<p>Newsletter body as HTML.</p>",
+        // ヘッダーではなく、冪等キーをフォームデータの一部とする。
+        "idempotency_key": uuid::Uuid::new_v4().to_string()
+    });
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // リダイレクトに従う
+    let html_page = app.get_publish_newsletter_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // *もう一度*、ニュースレターフォームを提出
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // リダイレクトに従う
+    let html_page = app.get_publish_newsletter_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"))
+
+    // ニュースレターメールを*1回だけ*送信したことを、モックはドロップした時に検証する。
+}
