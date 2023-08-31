5e634f947eea67e45e9a1582a8227cb20d9f14ab
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-08-31 17:05:45 +0900

    3.5.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
@@ -5,10 +7,10 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     Ok(
         HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-            .bind("127.0.0.1:8000")?
+            .listen(listener)?
             .run(),
     )
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,15 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // 準備する。
-    spawn_app();
+    let address = spawn_app();
     // アプリケーションに対してHTTPリクエストを実行するための`requwest`を持ち出す必要がある。
     let client = reqwest::Client::new();
 
     // 実行
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -17,8 +19,11 @@ async fn health_check_works() {
 }
 
 // バックグラウンドでアプリケーションを起動
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
-
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
