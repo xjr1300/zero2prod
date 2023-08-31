659c11cb4cff6446b1c6bc601e1bd0bedf084451
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-08-31 18:29:10 +0900

    3.7.3

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,16 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    Ok(
-        HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-            .listen(listener)?
-            .run(),
-    )
+    Ok(HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run())
 }
