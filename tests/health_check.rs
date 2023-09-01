7244ed2dc1809ace9496842547793ac3fab6baab
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-01 19:08:53 +0900

    3.8.5.4

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -26,7 +26,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to postgres");
     let client = reqwest::Client::new();
@@ -40,6 +40,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request");
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+    assert_eq!("ursula_le_guin@gmail.com", saved.email);
+    assert_eq!("Le Guin", saved.name);
 }
 
 #[tokio::test]
