9e12807489c1dbe1780b4a9605c53a687e3528dc
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-02 12:17:47 +0900

    3.9.2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
 use zero2prod::configuration::get_configuration;
-use zero2prod::run;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to database");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -79,7 +79,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
