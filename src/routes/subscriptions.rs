461e53ba5db538c41484ce5668cfd92bf850f170
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-04 11:48:47 +0900

    4.5.4

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -18,10 +18,8 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         subscriber_name = %form.name,
     );
     let _request_span_guard = request_span.enter();
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+    let _query_span_gurard = query_span.enter();
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
