abad1c93550181cf1048f091f065792d500f888d
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-04 16:36:40 +0900

    4.5.11

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,6 +2,7 @@ use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
+use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
@@ -12,10 +13,17 @@ use tracing_subscriber::{EnvFilter, Registry};
 /// 返されるサブスクライバーの実際の型を記述することを避けるために、戻り値の型に複雑な`impl Subscriber`を使用する。
 /// 返されるサブスクライバーは、後の`init_subscriber`に渡すことができるように、`Send`と`Sync`であるとを明示的に
 /// 示す必要がある。
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Send + Sync
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
 
     Registry::default()
         .with(env_filter)
