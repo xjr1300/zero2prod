e5c14e7dfbba4319d738b8ff746654b4520383c6
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-06 15:55:42 +0900

    6.14

--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -1,6 +1,6 @@
-use crate::domain::subscriber_name::SubscriberName;
+use crate::domain::{SubscriberEmail, SubscriberName};
 
 pub struct NewSubscriber {
-    pub email: String,
+    pub email: SubscriberEmail,
     pub name: SubscriberName,
 }
