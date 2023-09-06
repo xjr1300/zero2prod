811d1b1f52e9297be1f14ec5150f3523e05f98c1
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-06 16:17:01 +0900

    6.14.1

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,6 +12,17 @@ pub struct FormData {
     name: String,
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let email = SubscriberEmail::parse(value.email)?;
+        let name = SubscriberName::parse(value.name)?;
+
+        Ok(Self { email, name })
+    }
+}
+
 #[tracing::instrument(
     name="Adding a new subscriber",
     skip(form, pool),
@@ -21,15 +32,10 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
