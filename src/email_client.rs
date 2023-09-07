900103cfc4eb0679de06711be13dad0e973f0e9d
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-07 13:26:37 +0900

    7.2.5.1

--- src/email_client.rs
+++ src/email_client.rs
@@ -37,11 +37,11 @@ impl EmailClient {
         // この場合、anyhowクレートを導入するなど、複数のエラー型に対応する必要がある。
         let url = format!("{}/email", self.base_url);
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject,
+            html_body: html_content,
+            text_body: text_content,
         };
         self.http_client
             .post(url.as_str())
@@ -59,12 +59,12 @@ impl EmailClient {
 
 #[derive(serde::Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 #[cfg(test)]
