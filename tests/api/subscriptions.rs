bb89699d479058ab15d179d53a5374f58acbfedb
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-09 16:16:16 +0900

    7.7.2.1

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -80,3 +80,36 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
     // 主張
     assert_eq!(200, response.status().as_u16());
 }
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        // 私たちはここに予期回数を設定しない。
+        // テストはアプリの振る舞いの他の傾向に側面を合わせている。
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+        assert_eq!(links.len(), 1);
+
+        links[0].as_str().to_owned()
+    };
+
+    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(&body["TextBody"].as_str().unwrap());
+
+    assert_eq!(html_link, text_link);
+}
