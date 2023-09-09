1d6e310c5ac69737dc13cc6bd5c01447981d0672
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-09 16:47:43 +0900

    7.7.3.1

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -7,7 +7,6 @@ use crate::helpers::spawn_app;
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
     let body = "name=Le%20Guin&email=ursula_le_guin%40gmail.com";
-
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
@@ -16,13 +15,27 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     let response = app.post_subscriptions(body.into()).await;
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription");
     assert_eq!("ursula_le_guin@gmail.com", saved.email);
-    assert_eq!("Le Guin", saved.name);
+    assert_eq!("le guin", saved.name);
+    assert_eq!("pending_confirmation", saved.status);
 }
 
 #[tokio::test]
