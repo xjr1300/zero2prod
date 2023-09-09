d91f0914ee03754a12ad28a0d4f50a3fd5177d4f
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-09 15:26:08 +0900

    7.7.1.1

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,7 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
+use wiremock::MockServer;
 
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
@@ -22,6 +23,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -40,12 +42,16 @@ impl TestApp {
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration");
         // それぞれのテストケースで別のデータベースを使用
         c.database.database_name = format!("test_db_{}", Uuid::new_v4());
         // ランダムなOSポートを使用
         c.application.port = 0;
+        // EメールAPIとしてモックサーバーを使用
+        c.email_client.base_url = email_server.uri();
 
         c
     };
@@ -63,6 +69,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,3 +1,6 @@
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, ResponseTemplate};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
@@ -52,4 +55,21 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             description
         );
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    // 主張
 }
