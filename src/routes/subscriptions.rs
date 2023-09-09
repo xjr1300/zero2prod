6f8966c30d9394046ab5cb21b6f2d3bde33f5707
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-09 15:44:53 +0900

    7.7.1.2

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -5,6 +5,7 @@ use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
+use crate::email_client::EmailClient;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -25,21 +26,38 @@ impl TryFrom<FormData> for NewSubscriber {
 
 #[tracing::instrument(
     name="Adding a new subscriber",
-    skip(form, pool),
+    skip(form, pool, email_client),
     fields(
         subscriber_email=%form.email,
         subscriber_name=%form.name
     )
 )]
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+) -> HttpResponse {
     let new_subscriber = match form.0.try_into() {
         Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    match insert_subscriber(&pool, &new_subscriber).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
     }
+    if email_client
+        .send_email(
+            new_subscriber.email,
+            "Welcome!",
+            "Welcome to out newsletter!",
+            "Welcome to out newsletter!",
+        )
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
+    }
+
+    HttpResponse::Ok().finish()
 }
 
 /// もし、入力が購読者の名前としてすべての制約の検証を満たす場合は`true`、そうでなければ`false`を返す。
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -6,8 +6,14 @@ use crate::helpers::spawn_app;
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
-
     let body = "name=Le%20Guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
     let response = app.post_subscriptions(body.into()).await;
     assert_eq!(200, response.status().as_u16());
 
@@ -69,7 +75,8 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
         .mount(&app.email_server)
         .await;
 
-    app.post_subscriptions(body.into()).await;
+    let response = app.post_subscriptions(body.into()).await;
 
     // 主張
+    assert_eq!(200, response.status().as_u16());
 }
