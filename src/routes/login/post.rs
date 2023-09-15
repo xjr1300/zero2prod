1dcbbca7602dc225b03bf600693d1867c2467387
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-15 13:33:52 +0900

    10.6.3

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,7 +1,14 @@
 use actix_web::http::header::LOCATION;
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
+use secrecy::Secret;
 
-pub async fn login() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    username: String,
+    password: Secret<String>,
+}
+
+pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
         .finish()
