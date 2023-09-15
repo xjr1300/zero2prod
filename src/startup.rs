8e9f41674770d95c6a2b9cae2edbec5d21eaa63c
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-15 10:54:18 +0900

    10.5.1

--- src/startup.rs
+++ src/startup.rs
@@ -8,7 +8,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
+use crate::routes::{confirm, health_check, home, publish_newsletter, subscribe};
 
 pub struct Application {
     port: u16,
@@ -76,6 +76,7 @@ pub fn run(
     Ok(HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
