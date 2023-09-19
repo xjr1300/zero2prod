e3e251605046dbd47bb1d02772ec4ef2a883545a
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-19 16:09:37 +0900

    10.8.2.3

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,10 @@
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
+use crate::authentication::{validate_credentials, AuthError, Credentials};
+use crate::routes::admin::dashboard::get_username;
 use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 
@@ -14,16 +17,37 @@ pub struct FormData {
 
 pub async fn change_password(
     session: TypedSession,
+    pool: web::Data<PgPool>,
     form: web::Form<FormData>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(e500)?.is_none() {
+    let user_id = session.get_user_id().map_err(e500)?;
+    if user_id.is_none() {
         return Ok(see_other("/login"));
     }
+    let user_id = user_id.unwrap();
+
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
             "あなたは異なる2つの新しいパスワードを入力しました - 2つのフィールドの値は一致する必要があります。",
         ).send();
         return Ok(see_other("/admin/password"));
     }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password,
+    };
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("現在のパスワードが間違っています。").send();
+                Ok(see_other("/admin/password"))
+            }
+            AuthError::UnexpectedError(_) => Err(e500(e).into()),
+        };
+    }
+
     todo!()
 }
