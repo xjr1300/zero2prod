da5269f99d1485b749c47b31708e39c060b5f5ef
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-19 19:02:33 +0900

    10.9

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,9 @@
+use actix_web::error::InternalError;
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
+use uuid::Uuid;
 
 use crate::authentication::{validate_credentials, AuthError, Credentials};
 use crate::routes::admin::dashboard::get_username;
@@ -20,11 +22,7 @@ pub async fn change_password(
     pool: web::Data<PgPool>,
     form: web::Form<FormData>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id = session.get_user_id().map_err(e500)?;
-    if user_id.is_none() {
-        return Ok(see_other("/login"));
-    }
-    let user_id = user_id.unwrap();
+    let user_id = reject_anonymous_users(session).await?;
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
@@ -59,4 +57,15 @@ pub async fn change_password(
     FlashMessage::info("パスワードを変更しました。").send();
 
     Ok(see_other("/admin/password"))
+}
+
+async fn reject_anonymous_users(session: TypedSession) -> Result<Uuid, actix_web::Error> {
+    match session.get_user_id().map_err(e500)? {
+        Some(user_id) => Ok(user_id),
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("ユーザーはログインしていません。");
+            Err(InternalError::from_response(e, response).into())
+        }
+    }
 }
