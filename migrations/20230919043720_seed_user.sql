d912f1261110bbb2ba25d9e531a0074f62feb240
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-19 13:48:00 +0900

    10.8.1

---
+++ migrations/20230919043720_seed_user.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+    users (user_id, username, password_hash)
+VALUES
+    (
+        '9b4648dd-3515-4ff1-99bc-f420b2565ca5',
+        'admin',
+        /* cspell: disable-next-line */
+        '$argon2id$v=19$m=15000,t=2,p=1$LouLKvUOZNfuxWPPvnrhhQ$8//FlaXI82sGZgRxZq8iV8we8w0rXH7Yd1sfTdKZgPY'
+    );
