b18e3f613fb3bfe5a614a18f05df3de1915f296d
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-22 11:27:47 +0900

    11.8.3.1

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -1,3 +1,4 @@
+use actix_web::body::to_bytes;
 use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
 use sqlx::PgPool;
@@ -47,10 +48,26 @@ pub async fn get_saved_response(
 }
 
 pub async fn save_response(
-    _pool: &PgPool,
-    _idempotency_key: &IdempotencyKey,
-    _user_id: Uuid,
-    _http_response: &HttpResponse,
-) -> Result<(), anyhow::Error> {
-    todo!()
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+    http_response: HttpResponse,
+) -> Result<HttpResponse, anyhow::Error> {
+    let (response_head, body) = http_response.into_parts();
+    let status_code = response_head.status().as_u16() as i16;
+    let headers = {
+        let mut h = Vec::with_capacity(response_head.headers().len());
+        for (name, value) in response_head.headers().iter() {
+            let name = name.as_str().to_owned();
+            let value = value.as_bytes().to_owned();
+            h.push(HeaderPairRecord { name, value });
+        }
+        h
+    };
+
+    // TODO: SQL query
+
+    let http_response = response_head.set_body(body).map_into_boxed_body();
+
+    Ok(http_response)
 }
