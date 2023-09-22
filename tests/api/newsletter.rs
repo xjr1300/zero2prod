a40838dc8632cbd230df25aec66af73da7d0d440
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-22 13:52:01 +0900

    11.9.1

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use uuid::Uuid;
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
@@ -146,4 +148,34 @@ async fn newsletter_creation_is_idempotent() {
     let html_page = app.get_publish_newsletter_html().await;
     dbg!(&html_page);
     assert!(html_page.contains("ニュースレターの記事は発行されています。"));
+}
+
+#[tokio::test]
+async fn concurrent_form_submission_is_handled_gracefully() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200).set_delay(Duration::from_secs(2)))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "ニュースレターのタイトル",
+        "text_content": "テキストとしてのニューズレターの本文",
+        "html_content": "HTMLとしてのニュースレターの本文",
+        "idempotency_key": Uuid::new_v4().to_string(),
+    });
+    let response1 = app.post_publish_newsletter(&newsletter_request_body);
+    let response2 = app.post_publish_newsletter(&newsletter_request_body);
+    let (response1, response2) = tokio::join!(response1, response2);
+
+    assert_eq!(response1.status(), response2.status());
+    assert_eq!(
+        response1.text().await.unwrap(),
+        response2.text().await.unwrap()
+    );
 }
