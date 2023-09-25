0cde5cdd4bb4da27fa063a6037a9a3f301dbc9ed
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-09-25 11:07:50 +0900

    11.10.4.3

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -5,8 +5,6 @@ use sqlx::{Executor, PgPool, Postgres, Transaction};
 use uuid::Uuid;
 
 use crate::authentication::UserId;
-use crate::domain::SubscriberEmail;
-use crate::email_client::EmailClient;
 use crate::idempotency::{save_response, try_processing, IdempotencyKey, NextAction};
 use crate::utils::{e400, e500, see_other};
 
@@ -50,7 +48,6 @@ pub struct FormData {
 pub async fn publish_newsletter(
     pool: web::Data<PgPool>,
     user_id: web::ReqData<UserId>,
-    email_client: web::Data<EmailClient>,
     form: web::Form<FormData>,
 ) -> Result<HttpResponse, actix_web::Error> {
     let user_id = user_id.into_inner();
@@ -61,7 +58,7 @@ pub async fn publish_newsletter(
         idempotency_key,
     } = form.0;
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
-    let transaction = match try_processing(&pool, &idempotency_key, *user_id)
+    let mut transaction = match try_processing(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
     {
@@ -71,67 +68,25 @@ pub async fn publish_newsletter(
             return Ok(saved_response);
         }
     };
-    let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
-    for subscriber in subscribers {
-        match subscriber {
-            Ok(subscriber) => {
-                email_client
-                    .send_email(&subscriber.email, &title, &html_content, &text_content)
-                    .await
-                    .with_context(|| {
-                        format!(
-                            "ニュースレターの発行を{}に送信できませんでした。",
-                            subscriber.email
-                        )
-                    })
-                    .map_err(e500)?;
-            }
-            Err(e) => {
-                tracing::warn!(
-                    e.cause_chain = ?e,
-                    e.message = %e,
-                    "確認した購読者をスキップしました。蓄積された彼らの連絡先は不正です。"
-                );
-            }
-        }
-    }
-    success_message().send();
+    let issue_id = insert_newsletter_issue(&mut transaction, &title, &text_content, &html_content)
+        .await
+        .context("Failed to store newsletter issue details")
+        .map_err(e500)?;
+    enqueue_delivery_tasks(&mut transaction, issue_id)
+        .await
+        .context("Failed to enqueue delivery task")
+        .map_err(e500)?;
     let response = see_other("/admin/newsletters");
     let response = save_response(transaction, &idempotency_key, *user_id, response)
         .await
         .map_err(e500)?;
+    success_message().send();
 
     Ok(response)
 }
 
 fn success_message() -> FlashMessage {
     FlashMessage::info("ニュースレターの記事を発行しました。")
-}
-
-struct ConfirmedSubscriber {
-    email: SubscriberEmail,
-}
-
-async fn get_confirmed_subscribers(
-    pool: &PgPool,
-) -> anyhow::Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
-    let confirmed_subscribers = sqlx::query!(
-        r#"
-        SELECT email
-        FROM subscriptions
-        WHERE status = 'confirmed'
-        "#,
-    )
-    .fetch_all(pool)
-    .await?
-    .into_iter()
-    .map(|r| match SubscriberEmail::parse(r.email) {
-        Ok(email) => Ok(ConfirmedSubscriber { email }),
-        Err(e) => Err(anyhow::anyhow!(e)),
-    })
-    .collect();
-
-    Ok(confirmed_subscribers)
 }
 
 #[tracing::instrument(skip_all)]
